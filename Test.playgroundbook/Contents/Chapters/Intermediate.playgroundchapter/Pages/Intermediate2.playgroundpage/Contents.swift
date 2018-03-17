ccd45ee899053d465c4719d2d1222397bf187a44
Walter0807 <walterzhu@pku.edu.cn>
2018-03-17 11:12:43 +0800

    Rearrange

---
+++ Test.playgroundbook/Contents/Chapters/Intermediate.playgroundchapter/Pages/Intermediate2.playgroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//#-hidden-code
+import PlaygroundSupport
+//#-end-hidden-code
+let 📄 = Var(/*#-editable-code*/true/*#-end-editable-code*/), 🖋 = Var(/*#-editable-code*/true/*#-end-editable-code*/)
+let 📝 = AND(📄,🖋)
+
+let 🍦 = Var(/*#-editable-code*/true/*#-end-editable-code*/), 🍨 = Var(/*#-editable-code*/true/*#-end-editable-code*/)
+let 😍 = OR(🍦,🍨)
+
+let 👍 = Var(/*#-editable-code*/true/*#-end-editable-code*/)
+let 👎 = NOT(👍)
